6bff34a3dc5e397ac94c877ab0bee1cbe5383756
UJKEM <eminemism2ujjwal@gmail.com>
2022-01-18 20:25:03 +0530

    Node 12th commit

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ app.use(
 app.use(cors());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://reactscrapper.herokuapp.com"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
