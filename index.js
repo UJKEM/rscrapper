4d4a062409a1c3ce4a32aa9e320b2efaa40fed65
UJKEM <eminemism2ujjwal@gmail.com>
2022-01-18 20:16:29 +0530

    Node 10th commit

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ db.connection.connect(function (err) {
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
